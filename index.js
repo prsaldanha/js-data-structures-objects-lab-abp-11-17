e43fb011679f610e2d98d92bcb047fab1c27f5b8
prem_saldanha@yahoo.ca <prem_saldanha@yahoo.ca>
2017-11-12 19:13:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var driver = {};
 
 function updateDriverWithKeyAndValue(driver, key, value)
 {
-  return Object.assign(driver, {key: value});
+  driver.name = key;
+  driver.address = value;
+  return Object.assign(...driver);
 
 }
